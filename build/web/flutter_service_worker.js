30198998b01c9f1ccafe32936260bf4c5a200152
Carlos Velasco <carlosvelasco21347@gmail.com>
2025-10-21 22:42:52 -0500

    cambios y mas cambios

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7a6082424b485cd86ba7882c9c6d159e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ba2b4593a1cddb00c4397919ba698329",
+"flutter_bootstrap.js": "a6e446742315f8cdfdc6bd0b21b2d445",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
